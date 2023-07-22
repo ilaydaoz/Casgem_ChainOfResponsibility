0d00597dc145bc47e1f2c175291f83ab496d9910
ilayda <ilaydaozken@gmail.com>
2023-07-22 10:00:53 +0300

    Add project files.

---
+++ Casgem_ChainOfResponsibility/Dal/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_ChainOfResponsibility.Dal
+{
+    public class Context:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-AKOHCIC;initial catalog=Casgem_ChainOfResponsibility;integrated security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
---
+++ Casgem_ChainOfResponsibility/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Casgem_ChainOfResponsibility.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
